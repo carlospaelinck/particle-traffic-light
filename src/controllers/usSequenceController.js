2403bd36b5efef0ab2221d70dca5187b94ad8ffe
Carlos Paelinck <carlos@carlospaelinck.com>
2015-09-12 13:27:34 -0500

    Added light timings controller

--- src/controllers/usSequenceController.js
+++ src/controllers/usSequenceController.js
@@ -9,16 +9,17 @@ import {assign} from 'lodash';
 export default class UsSequenceController {
   constructor($scope, ParticleService) {
     assign(this, {$scope, ParticleService});
-  }
 
-  performSequence() {
-    let sequenceOptions = {
+    this.timers = {
       red: 4,
       yellow: 2,
       green: 4,
-      repeat: 2,
-      uk: false
+      repeat: 1
     };
+  }
+
+  performSequence() {
+    let sequenceOptions = _.extend(this.timers, {uk: false});
 
     this.ParticleService.lightSequence(sequenceOptions)
       .then(() => {
